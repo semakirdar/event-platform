e4fcc33ec0a5f8facb5e0616470877c4bfd20c3c
Sema Kırdar <sema@dijitaladam.com>
2021-10-11 14:30:36 +0300

    migrations created

---
+++ database/migrations/2021_10_11_112358_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->string('name');
+            $table->string('title');
+            $table->string('company');
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+require('./bootstrap');
+require('@popperjs/core');
+require('bootstrap');
+toastr = require('toastr');
